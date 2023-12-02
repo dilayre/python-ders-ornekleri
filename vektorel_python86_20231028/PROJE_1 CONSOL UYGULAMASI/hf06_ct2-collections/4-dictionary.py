e47af476ae3282504d63c02ab15764e936eb7124
Dilara <107860010+dilayre@users.noreply.github.com>
2023-12-02 11:27:24 +0300

    set örnek

--- vektorel_python86_20231028/PROJE_1 CONSOL UYGULAMASI/hf06_ct2-collections/4-dictionary.py
+++ vektorel_python86_20231028/PROJE_1 CONSOL UYGULAMASI/hf06_ct2-collections/4-dictionary.py
@@ -4,3 +4,21 @@ sozluk = {
     "köpek":"dog",
     "kelebek":"butterfly"
 }
+
+arabaListesi = {
+    "markası":"toyota",
+    "fiyatı":"400000"
+}
+print(arabaListesi["markası"])
+
+
+
+for zz in sozluk:
+    print("index yerine",zz,"var ve değeri: ",sozluk[zz])
+
+arabalar = {
+    "araba1": {"marka":"toyota","fiyatı":400000},
+    "araba2": {"marka":"opel","fiyatı":500000},
+    "araba3": {"marka":"mercedes","fiyatı":600000}
+}
+print(arabalar["araba3[marka]"])
