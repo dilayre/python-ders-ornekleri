dda62b2ad1f552ca13b3cf9bbcde0c3c8473a6d8
Dilara <107860010+dilayre@users.noreply.github.com>
2023-12-02 12:33:21 +0300

    searching_2

--- vektorel_python86_20231028/PROJE_1 CONSOL UYGULAMASI/hf06_ct3_siniflar ve searching/2-search.py
+++ vektorel_python86_20231028/PROJE_1 CONSOL UYGULAMASI/hf06_ct3_siniflar ve searching/2-search.py
@@ -1,9 +1,15 @@
 import random
-sayilar = []
-for a in range(10000000):
-    sayilar.append(random.randint(10,99))
-x = 1
-print(x,".sayi",sayilar[x])
+# sayilar = []
+# for a in range(10000000):
+#     sayilar.append(random.randint(10,99))
+# x = 1
+# print(x,".sayi",sayilar[x])
 
-x = 898532
-print(x,".sayi",sayilar[x])
+# x = 898532
+# print(x,".sayi",sayilar[x])
+
+vatandaslar = []
+for a in range(10000):
+    vatandaslar.append(random.randint(10,99))
+for xx in vatandaslar:
+    print(xx)
