315b50da02460bf75df1898afc2e3e10b9a5505c
Dilara <107860010+dilayre@users.noreply.github.com>
2023-12-03 10:04:34 +0300

    tekrar

---
+++ vektorel_python86_20231028/PROJE_1 CONSOL UYGULAMASI/hf06_pz_tekrar/1-diziler.py
@@ -0,0 +1,15 @@
+araclar1 = ["ferrari","doğan","bmw"]
+araclar2 = ("şahin","Ford","TOGG")
+araclar3 = {"Mercedes","Mazda","Volvo"} #set türünde ve indisi yok
+araclar4 = {"Kıvanç":"Hyundai","Uğurhan":"Mercedes","Ahmet":"Honda"} #dictionary
+print(araclar1)
+print(araclar2)
+print(araclar3)
+print(araclar4)
+print("==========================")
+
+print(araclar1[1])
+print(araclar2[1])
+# print(araclar3[1]) #bu şekilde kullanımı hatalıdır
+print(araclar4["Ahmet"])
+
